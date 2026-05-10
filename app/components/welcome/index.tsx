86acff5b439930ed1c17f711f43f91687a65c5b1
Ahmad taha <139067507+Ahmadshante11@users.noreply.github.com>
2026-05-10 19:48:29 +0300

    Update index.tsx

--- app/components/welcome/index.tsx
+++ app/components/welcome/index.tsx
@@ -2,13 +2,10 @@
 import type { FC } from 'react'
 import React, { useEffect, useState } from 'react'
 import { useTranslation } from 'react-i18next'
-import TemplateVarPanel, { PanelTitle, VarOpBtnGroup } from '../value-panel'
+import TemplateVarPanel from '../value-panel'
 import s from './style.module.css'
 import { AppInfoComp, ChatBtn } from './massive-component'
 import type { AppInfo, PromptConfig } from '@/types/app'
-import Toast from '@/app/components/base/toast'
-import Select from '@/app/components/base/select'
-import { DEFAULT_VALUE_MAX_LEN } from '@/config'
 
 export interface IWelcomeProps {
   conversationName: string
@@ -23,7 +20,6 @@ export interface IWelcomeProps {
 }
 
 const Welcome: FC<IWelcomeProps> = ({
-  conversationName,
   hasSetInputs,
   siteInfo,
   promptConfig,
@@ -39,38 +35,6 @@ const Welcome: FC<IWelcomeProps> = ({
     return res
   })())
 
-  const { notify } = Toast
-  const logError = (message: string) => { notify({ type: 'error', message, duration: 3000 }) }
-
-  const renderInputs = () => (
-    <div className='space-y-3'>
-      {promptConfig.prompt_variables.map(item => (
-        <div className='tablet:flex items-start mobile:space-y-2 tablet:space-y-0 mobile:text-xs tablet:text-sm' key={item.key}>
-          <label className={`flex-shrink-0 flex items-center tablet:leading-9 mobile:text-gray-700 tablet:text-gray-900 mobile:font-medium pc:font-normal ${s.formLabel}`}>{item.name}</label>
-          {item.type === 'select' && (
-            <Select
-              className='w-full'
-              defaultValue={inputs?.[item.key]}
-              onSelect={(i) => { setInputs({ ...inputs, [item.key]: i.value }) }}
-              items={(item.options || []).map(i => ({ name: i, value: i }))}
-              allowSearch={false}
-              bgClassName='bg-gray-50'
-            />
-          )}
-          {item.type === 'string' && (
-            <input
-              placeholder={`${item.name}${!item.required ? `(${t('app.variableTable.optional')})` : ''}`}
-              value={inputs?.[item.key] || ''}
-              onChange={(e) => { setInputs({ ...inputs, [item.key]: e.target.value }) }}
-              className={'w-full flex-grow py-2 pl-3 pr-3 box-border rounded-lg bg-gray-50'}
-              maxLength={item.max_length || DEFAULT_VALUE_MAX_LEN}
-            />
-          )}
-        </div>
-      ))}
-    </div>
-  )
-
   const handleChat = () => {
     onStartChat(inputs)
   }
@@ -80,7 +44,6 @@ const Welcome: FC<IWelcomeProps> = ({
       <div className='mx-auto pc:w-[794px] max-w-full mobile:w-full px-3.5'>
         <div className='mobile:pt-[72px] tablet:pt-[128px] pc:pt-[200px]'>
           <TemplateVarPanel isFold={false} header={<AppInfoComp siteInfo={siteInfo} />}>
-            {hasVar && renderInputs()}
             <ChatBtn className={hasVar ? 'mt-3 mobile:ml-0 tablet:ml-[128px]' : ''} onClick={handleChat} />
           </TemplateVarPanel>
         </div>
