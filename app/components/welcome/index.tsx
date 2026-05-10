c2acb421b24ea4c47879dd8dd09156defc864cab
Ahmad taha <139067507+Ahmadshante11@users.noreply.github.com>
2026-05-10 16:57:27 +0300

    Update index.tsx

--- app/components/welcome/index.tsx
+++ app/components/welcome/index.tsx
@@ -387,10 +387,9 @@ const Welcome: FC<IWelcomeProps> = ({
               </div>
               : <div>
               </div>}
-            <a className='flex items-center pr-3 space-x-3' href="https://dify.ai/" target="_blank">
-              <span className='uppercase'>{t('app.chat.powerBy')}</span>
-              <FootLogo />
-            </a>
+          <a className='flex items-center pr-3 space-x-3' href="https://medicoai.online/" target="_blank">
+  <span className='uppercase'>Developed by MedcoAI</span>
+</a>
           </div>
         )}
       </div>
